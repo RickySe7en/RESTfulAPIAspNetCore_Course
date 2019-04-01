85b0f0d9cf712d9dab635f8ce2c69995978f51eb
RickySe7en <sshu923@aucklanduni.ac.nz>
2019-04-02 02:42:18 +1300

    add authors controller first api working

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,22 @@
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
