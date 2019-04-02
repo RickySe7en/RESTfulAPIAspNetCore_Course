d3b47ce61cbf0f6ab466c31868adbee72c21a843
RickySe7en <sshu923@aucklanduni.ac.nz>
2019-04-02 23:31:50 +1300

    add author collection

--- src/Library.API/Models/AuthorDto.cs
+++ src/Library.API/Models/AuthorDto.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace Library.API.Models
 {
     public class AuthorDto
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
         public string Genre { get; set; }
